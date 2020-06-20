03c4d67bba3ea2e63842c0489e94a9c1a4ec37a1
TaurVil <41305538+TaurVil@users.noreply.github.com>
2020-06-19 19:31:14 -0500

    Add files via upload

---
+++ run.03.merge_combine_neutral.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+index=${SLURM_ARRAY_TASK_ID}
+
+n=`head -$index 01_chroms | tail -1`
+#head -$n genome.35Mb.bed | tail -1 > tmp.$n.bed
+
+
+module load java; module load samtools; module load python
+module load htslib
+
+/project2/lbarreiro/users/tauras/Programs/gatk-4.1.4.1/gatk CombineGVCFs -R hg19/hg19.fa -O $n.neutral.g.vcf.gz -V 01_neutral_gvcf.list -L $n
+
+/project2/lbarreiro/users/tauras/Programs/gatk-4.1.4.1/gatk GenotypeGVCFs -V $n.neutral.g.vcf.gz -R hg19/hg19.fa -O $n.neutral.vcf.gz --tmp-dir /scratch/midway2/rdd6247/
+
